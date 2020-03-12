539c28e63a1b4b94a8110f0388c2fb32feb77afc
Robin Hall <robinh@Robins-MacBook-Air.local>
2020-03-12 09:41:00 +0200

    further automation, able to create master pipeline script now.

--- PythonProjects/Benchmarking/create_pipeline.py
+++ PythonProjects/Benchmarking/create_pipeline.py
@@ -28,6 +28,7 @@ def write_batch(filename, script, script_args, container_path, *args):
     content = content.format(**params).replace("    ", "")
     sbatch_file = open(filename, 'a')
     sbatch_file.write(content)
+    sbatch_file.close()
 
 
 def write_batch_all(scripts, script_args, container_path, *args):
@@ -38,13 +39,37 @@ def write_batch_all(scripts, script_args, container_path, *args):
     :param args: sbatch arguments
     :return:
     """
+    script_list = []
     for i, script in enumerate(scripts):
-        filename = script + '_sbatch.sh'
+        filename = str(script).replace('.py', '') + '_sbatch.sh'
+        script_list.append(filename)
         write_batch(filename, script, script_args[i], container_path, args[0], args[1])
 
+    return script_list
 
-scripts = ['script1.py', 'script2.py', 'script3.py', 'script4.py']
-script_args = ['32 test_script_creation.log', '32 test_script_creation.log', '32 test_script_creation.log', '32 test_script_creation.log']
-container_path = '/some/path/container.simg'
 
-write_batch_all(scripts, script_args, container_path, 32, 'test_func')
+def write_pipeline(script_list, filename):
+    """
+    :param script_list: list of batch scripts (returned by write_batch_all() function)
+    :param filename: name of file to be written
+    :return:
+    """
+    pipeline_script = open(filename, 'a')
+    pipeline_script.write('#!/bin/bash\n')
+    pipeline_script.write('\n')
+    pipeline_script.write('jid0=$(sbatch ' + script_list[0] + ')\n')
+    pipeline_script.write('jid0=${jid0##* }\n')
+    for i, script in enumerate(script_list):
+        pipeline_script.write('jid' + str(i + 1) + '=$(sbatch --dependency=afterok:$jid' + str(i) + ' ' +
+                              script_list[i + 1] + ')\n')
+        pipeline_script.write('jid' + str(i + 1) + '=${jid' + str(i + 1) + '##* }\n')
+
+
+if __name__ == '__main__':
+    scripts = ['script1.py', 'script2.py', 'script3.py', 'script4.py']
+    script_args = ['32 test_script_creation.log', '32 test_script_creation.log', '32 test_script_creation.log',
+                   '32 test_script_creation.log']
+    container_path = '/some/path/container.simg'
+
+    batch_list = write_batch_all(scripts, script_args, container_path, 32, 'test_func')
+    write_pipeline(batch_list, 'pipeline.sh')
